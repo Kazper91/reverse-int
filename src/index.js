19adcfd98de561177a6933ae5a3d5996b7a06aee
Kazper91 <kazper91@gmail.com>
2023-02-05 23:57:10 +0300

    fix: add nevative number check

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 module.exports = function reverse (n) {
+    if (n < 0) {
+        n = -n;
+    } else n = n;
+    
     let toString = String(n);
     let splitString = toString.split("");
     let reverseString = splitString.reverse("");
