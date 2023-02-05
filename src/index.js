eca40a257997a07b1d2d3013ac5aab12fc8c69f5
Kazper91 <kazper91@gmail.com>
2023-02-05 23:48:51 +0300

    feat: complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let toString = String(n);
+    let splitString = toString.split("");
+    let reverseString = splitString.reverse("");
+    let newString = reverseString.join("");
+    let reverseNumber = +newString;
+    return reverseNumber;
 }
